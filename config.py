108dcad6566edc9ae9431c7d8c50531ad0f595fd
alexesking <126413425+alexesking@users.noreply.github.com>
2024-02-02 15:51:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/2f7d46d6b87635fe01d93.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/2f7d46d6b87635fe01d93.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EE_47")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EE_20")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/E0_0_0")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T_T_T_U")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5801201416").split()))
 
 
-FAILED = "https://graph.org/file/6e5004198c3bdbba84bdc.jpg"
+FAILED = "https://telegra.ph/file/2f7d46d6b87635fe01d93.jpg"
